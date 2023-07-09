90bf2174b061fdf079a220566b6df17feb52aa9c
Wexop <mattheurtin@gmail.com>
2023-07-09 23:58:45 +0200

    no bounce chelou

--- Assets/Scritps/PlayerJump.cs
+++ Assets/Scritps/PlayerJump.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private Animator _animator;
     [SerializeField] private float jumpPower;
     [SerializeField] private float jumpCd;
     private float _lastJump;
+    private static readonly int Jump1 = Animator.StringToHash("Jump");
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _lastJump = jumpCd;
     }
 
     // Update is called once per frame
     void Update()
     {
         _lastJump += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        if (Input.GetKey(KeyCode.Space)) Jump();
     }
 
     public void Jump()
     {
         if (_lastJump < jumpCd) return;
+        _lastJump = 0;
         _rb.velocity = new Vector2(0, jumpPower);
+        _animator.SetTrigger(Jump1);
     }
 }
